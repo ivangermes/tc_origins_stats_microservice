77358acd3769b70aeb63effb8073e957be6fb5d2
ivan <ivan@levelupdev.com>
2023-11-10 02:34:31 +0300

    made the tests up-to-date

--- test.sh
+++ test.sh
@@ -1,22 +1,18 @@
 HOST="http://0.0.0.0:8082"
 
 set -x
-curl -w "\n\n" -X GET  "$HOST/health"
+curl -i -w "\n\n" -X GET  "$HOST/health"
 
-curl -w "\n\n" -X GET  "$HOST/v1/origins_stats"
-curl -w "\n\n" -X POST "$HOST/v1/origins_stats"
+curl -i -w "\n\n" -X GET  "$HOST/v1/origins_stats"
 
-curl -w "\n\n" -X GET  "$HOST/v1/origins_stats?time_from=2023-04-06T23:27:54.000Z&time_to=2023-04-08T07:11:04.000Z"
-curl -w "\n\n" -X POST "$HOST/v1/origins_stats" -F "time_from=2023-04-06T23:27:54.000Z" -F "time_to=2023-04-08T07:11:04.000Z"
+curl -i -w "\n\n" -X GET  "$HOST/v1/origins_stats?time_from=2023-04-06T23:27:54.000Z&time_to=2023-04-08T07:11:04.000Z"
 
-curl -w "\n\n" -X GET  "$HOST/v1/origins_stats?time_from=2023-04-06T23:27:54.000Z"
-curl -w "\n\n" -X POST "$HOST/v1/origins_stats" -F "time_from=2023-04-06T23:27:54.000Z"
+curl -i -w "\n\n" -X GET  "$HOST/v1/origins_stats?time_from=2023-04-06T23:27:54.000Z"
 
-curl -w "\n\n" -X GET  "$HOST/v1/origins_stats?time_to=2023-04-08T07:11:04.000Z"
-curl -w "\n\n" -X POST "$HOST/v1/origins_stats" -F "time_to=2023-04-08T07:11:04.000Z"
+curl -i -w "\n\n" -X GET  "$HOST/v1/origins_stats?time_to=2023-04-08T07:11:04.000Z"
 
-curl -w "\n\n" -X GET  "$HOST/v1/origins_stats?time_from=2023-04-08T07:11:04.000Z&time_to=2023-04-06T23:27:54.000Z"
-curl -w "\n\n" -X POST "$HOST/v1/origins_stats" -F "time_from=2023-04-08T07:11:04.000Z" -F "time_to=2023-04-06T23:27:54.000Z"
+curl -i -w "\n\n" -X GET  "$HOST/v1/origins_stats?time_from=2023-04-06&time_to=2023-04-08"
 
-curl -w "\n\n" -X GET  "$HOST/v1/origins_stats?time_from=2023-04-06&time_to=2023-04-08"
-curl -w "\n\n" -X POST "$HOST/v1/origins_stats" -F "time_from=2023-04-06" -F "time_to=2023-04-08"
+curl -i -w "\n\n" -X GET  "$HOST/v1/origins_stats?time_from=2023-04-08T07:11:04.000Z&time_to=2023-04-06T23:27:54.000Z"
+
+curl -i -w "\n\n" -X GET  "$HOST/v1/origins_stats?time_to=2023-99-08T07:11:04.000Z"
